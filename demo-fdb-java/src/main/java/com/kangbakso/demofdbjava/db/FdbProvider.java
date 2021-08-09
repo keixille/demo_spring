d00ee834fe00f6d635242000dd70c515f7d701a1
Dian Aditya Nugraha <dian.aditya.n@gmail.com>
2021-08-09 14:26:47 +0700

    remove comment

--- demo-fdb-java/src/main/java/com/kangbakso/demofdbjava/db/FdbProvider.java
+++ demo-fdb-java/src/main/java/com/kangbakso/demofdbjava/db/FdbProvider.java
@@ -11,12 +11,10 @@ public class FdbProvider {
     private static final FDB fdb;
     public static final Database db;
     public static final Subspace userSubspace;
-//    public static final Subspace index;
 
     static {
         fdb = FDB.selectAPIVersion(630);
         db = fdb.open();
         userSubspace = new Subspace(Tuple.from("user_subspace"));
-//        index = new Subspace(Tuple.from("zipcode_index"));
     }
 }
